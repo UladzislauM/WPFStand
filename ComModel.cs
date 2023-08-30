4f185c292f11473e99c378f057811eeabc99a2ef
UladzislauM <marshal25@yandex.by>
2023-08-30 06:54:57 +0300

    initial commit

---
+++ ComModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStandApp
+{
+    class ComModel
+    {
+       private string id { get; set; }
+       private string data { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ComModel model &&
+                   id == model.id &&
+                   data == model.data;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id, data);
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace TestStandApp
+{
+    public partial class MainWindow : Window
+    {
+        private Dictionary<String, String> commands = new Dictionary<string, string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillSerialPortComboBox();
+            CreateCommandsForDictionary();
+            AddDataToComboBox();
+        }
+
+        private void Test_send_Click(object sender, RoutedEventArgs e)
+        {
+            SerialPort port = new SerialPort(serialPortComboBox.SelectedItem.ToString() ?? "Empty port",
+          19200, Parity.None, 8, StopBits.One);
+            try
+            {
+
+                port.Open();
+
+                byte[] outByteArray = ReadData();
+
+                port.Write(outByteArray, 0, outByteArray.Length);
+
+                byte[] inData = new byte[port.BytesToRead];
+                port.Read(inData, 0, inData.Length);
+
+                in_data.Text = ByteArrayToFormattedString(inData);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exxx:" + ex.Message);
+            }
+            finally
+            {
+                port.Close();
+            }
+
+        }
+
+        private string ByteArrayToFormattedString(byte[] byteArray)
+        {
+            string formattedString = "";
+
+            foreach (byte b in byteArray)
+            {
+                formattedString += "0x" + b.ToString("X2") + " ";
+            }
+
+            return formattedString.Trim();
+        }
+
+        private byte[] ReadData()
+        {
+            string ff = Entered_command.Text;
+            if (Entered_command.Text.Equals("Enter command") || Entered_command.Text.Equals(" "))
+            {
+                string keysCommands = CommandsBox.SelectedItem.ToString() ?? "Empty";
+                string command = commands[keysCommands];
+                byte[] byteArray = command.Split(' ').Select(s => Convert.ToByte(s, 16)).ToArray();
+                byteArray = byteArray.Concat(new byte[] { CalculateLRC(byteArray) }).ToArray();
+                return byteArray;
+            }
+            else
+            {
+                string input = Entered_command.Text;
+                byte[] byteArray = input.Split(' ').Select(s => Convert.ToByte(s, 16)).ToArray();
+                return byteArray;
+            }
+        }
+
+        private void FillSerialPortComboBox()
+        {
+            string[] portNames = SerialPort.GetPortNames();
+            foreach (string portName in portNames)
+            {
+                serialPortComboBox.Items.Add(portName);
+            }
+        }
+
+        private void CreateCommandsForDictionary()
+        {
+            commands = new Dictionary<string, string>
+            {
+                { "Scan", "0xDC 0x04 0x28 0x02 0x058" },
+                { "Starting position", "0xDC 0x02 0x2D" },
+                { "Run with 200 speed", "0xDC 0x04 0x01 0x02 0x00" },
+                { "Light on", "0xDC 0x04 0x03 0x01 0x00" },
+                { "Status", "0xDC 0x02 0x0B" },
+                { "Exceptions?", "0xDC 0x02 0x0A" }
+            };
+        }
+
+        private byte CalculateLRC(byte[] data)
+        {
+            byte lrc = 0x00;
+            foreach (byte b in data)
+            {
+                lrc ^= b;
+            }
+            return lrc;
+        }
+
+        private void AddDataToComboBox()
+        {
+            string[] commandsNames = commands.Keys.ToArray();
+            foreach (string commandName in commandsNames)
+            {
+                CommandsBox.Items.Add(commandName);
+            }
+        }
+
+        private void Entered_command_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Entered_command.Text.Equals("Enter commnd"))
+            {
+                Entered_command.Clear();
+            }
+        }
+    }
+}
